2e9e001e839bf659945c2ee7aedf6eda4619c533
Willard Wong <ifyour@outlook.com>
2020-04-22 05:00:05 +0000

    fix: use query string

--- api/cors.js
+++ api/cors.js
@@ -7,9 +7,6 @@ const cors_proxy = require('./lib/cors-anywhere').createServer(options);
 
 
 module.exports = (req, res) => {
-  
-  // ZEIT Bug: `/https://xxx.com` -> `/https:/xxx.com`
-  req.url = req.url.replace(':/', '://');
-  
+  req.url = `/${req.query.url || ''}`;
   cors_proxy.emit('request', req, res);
 };
