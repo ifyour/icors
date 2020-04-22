04772a0aa57803c960d4ad3c556522f83ff62132
Willard Wong <ifyour@outlook.com>
2020-04-22 16:19:41 +0800

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,13 +1,11 @@
-const options = {
+const cors_proxy = require("./lib/cors-anywhere").createServer({
   originWhitelist: [],
   requireHeader: [],
   removeHeaders: [],
-};
-const cors_proxy = require('./lib/cors-anywhere').createServer(options);
-
+});
 
 module.exports = (req, res) => {
   const [_url] = Object.keys(req.query);
-  req.url = `/${_url || ''}`;
-  cors_proxy.emit('request', req, res);
+  req.url = `/${_url || ""}`;
+  cors_proxy.emit("request", req, res);
 };
