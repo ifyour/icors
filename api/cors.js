08f9de87112d747a07dde0465d493f338e487635
Willard Wong <ifyour@outlook.com>
2020-04-22 10:10:27 +0800

    fix: tweak

--- api/cors.js
+++ api/cors.js
@@ -7,9 +7,9 @@ const cors_proxy = require('./lib/cors-anywhere').createServer(options);
 
 
 module.exports = (req, res) => {
-
-  // fix: Error: getaddrinfo ENOTFOUND https
-  const url = Object.assign(req, { url: req.url.replace(':/', '://') });
-
-  cors_proxy.emit('request', url, res);
+  
+  // ZEIT Bug: `/https://xxx.com` -> `/https:/xxx.com`
+  req.url = req.url.replace(':/', '://');
+  
+  cors_proxy.emit('request', req, res);
 };
