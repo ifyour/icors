884063401b4a996968cd716250346fa452e5bc1a
Willard Wong <ifyour@outlook.com>
2020-04-22 08:36:39 +0000

    fix: build error

--- api/cors.js
+++ api/cors.js
@@ -1,8 +1,9 @@
-const cors_proxy = require("./lib/cors-anywhere").createServer({
+const options = {
   originWhitelist: [],
   requireHeader: [],
   removeHeaders: [],
-});
+};
+const cors_proxy = require("./lib/cors-anywhere").createServer(options);
 
 module.exports = (req, res) => {
   const [_url] = Object.keys(req.query);
