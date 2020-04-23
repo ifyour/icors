876f512a9d440254a6c0e6ee9b3fc9d6446bf6fd
Willard Wong <ifyour@outlook.com>
2020-04-23 01:06:09 +0000

    fix: url error

--- api/cors.js
+++ api/cors.js
@@ -2,10 +2,14 @@ const cors_proxy = require("cors-anywhere").createServer({
   originWhitelist: [],
   requireHeader: [],
   removeHeaders: [],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    xfwd: true
+  }
 });
 
 module.exports = (req, res) => {
-  const [_url] = Object.keys(req.query);
-  req.url = `/${_url || ""}`;
+  const _url = decodeURIComponent(req.url).replace(/\?/, '');
+  req.url = `${_url || ''}`;
   cors_proxy.emit("request", req, res);
 };
